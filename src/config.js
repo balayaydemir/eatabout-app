0680dcac40658315e684fc2d8eecd6c7d27633ef
balayaydemir <balayaydemir@gmail.com>
2019-11-24 21:38:28 -0800

    fixed stuff

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     API_KEY: process.env.REACT_APP_API_KEY,
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    API_ENDPOINT: 'https://infinite-plateau-44415.herokuapp.com',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
     IMG_SRC: process.env.REACT_APP_IMG_SRC
 }
