c6fc860d6183e6d2c03d58ba78861e1eb468d96f
balayaydemir <balayaydemir@gmail.com>
2020-01-27 12:38:26 -0800

    form validation for signup done

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,60 +1,94 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AuthApiService from '../../services/auth-api-service';
+import useFormValidation from '../../hooks/useFormValidation';
+import validateInput from '../../hooks/validateInput';
+import swal from 'sweetalert';
 import './SignupForm.css';
 
-export default class SignupForm extends Component {
-    static defaultProps = {
-        registrationSuccess: () => { }
-    }
+const SignupForm = (props) => {
+    const inputValues = {
+        username: '',
+        password: ''
+      };
 
-    state = {
-        error: null
-    }
+    const handleSignUp = () => {
 
+        const { fullname, username, password } = values
 
-    handleSubmit = e => {
-        e.preventDefault()
-        const { full_name, user_name, password } = e.target
-
-        this.setState({ error: null })
         AuthApiService.postUser({
-            user_name: user_name.value,
-            password: password.value,
-            full_name: full_name.value
+            user_name: username,
+            password: password,
+            full_name: fullname
         })
-            .then(user => {
-                full_name.value = ''
-                user_name.value = ''
-                password.value = ''
-                this.props.registrationSuccess()
+            .then(() => {
+                props.registrationSuccess()
             })
-            .catch(res => {
-                this.setState({ error: res.error })
+            .catch(error => {
+                swal({
+                    title: 'Uh oh!',
+                    text: error.error,
+                    icon: 'error',
+                    button: true
+                })
             })
 
     }
-    render() {
-        const { error } = this.state
+
+    const {
+        handleSubmit,
+        errors,
+        handleChange,
+        values,
+        handleBlur,
+        isSubmitting,
+      } = useFormValidation(
+        inputValues,
+        validateInput.validateSignup,
+        handleSignUp
+      );
+
         return (
-            <form className='SignUpForm' onSubmit={this.handleSubmit}>
-                <div role='alert'>
-                    {error && <p className='red'>{error}</p>}
-                </div>
-                <div>
-                    <label htmlFor="SignUpForm__user_name">Username: </label>
-                    <input placeholder='User Name' type="text" name='user_name' id='SignUpForm__user_name' />
-                </div>
-                <div>
-                    <label htmlFor="SignUpForm__full_name">Full name: </label>
-                    <input type="text" name='full_name' id='SignUpForm__full_name' placeholder='Full Name' />
-                </div>
-                <div>
-                    <label htmlFor="SignUpForm__password">Password: </label>
-                    <input type="password" name='password' id='SignUpForm__password' />
-                    {this.props.capsLock ? <strong>Caps Lock is On!</strong> : ''}
-                </div>
-                <button type='submit' className="submit">Sign Up</button>
+            <form className='SignUpForm' onSubmit={handleSubmit}>
+                <label htmlFor="username">
+                    Username: 
+                    <input 
+                        type="text"
+                        name="username"
+                        onChange={handleChange}
+                        value={values.username}
+                        onBlur={handleBlur}
+                        placeholder="Username"
+                    />
+                </label>
+                {errors.username && <span className="form_error">*{errors.username}</span>}
+                <label htmlFor="fullname">
+                    Full Name: 
+                    <input 
+                        type="text"
+                        name="fullname"
+                        onChange={handleChange}
+                        value={values.fullname}
+                        onBlur={handleBlur}
+                        placeholder="Full Name"
+                    />
+                </label>
+                {errors.fullname && <span className="form_error">*{errors.fullname}</span>}
+                <label htmlFor="password">
+                    Password: 
+                    <input 
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        value={values.password}
+                        onBlur={handleBlur}
+                        placeholder="Password"
+                    />
+                </label>
+                {errors.password && <span className="form_error">*{errors.password}</span>}
+                {props.capsLock ? <strong>Caps Lock is On!</strong> : ''}
+                <button type='submit' className="submit" disabled={isSubmitting}>Sign Up</button>
             </form>
         )
     }
-}
+
+    export default SignupForm
